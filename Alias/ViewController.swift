e48ba82c33f08486f7098f4695996504c0e0f371
alexKozachuk <grayid@icloud.com>
2020-07-08 11:07:27 +0300

    Initial Commit

---
+++ Alias/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alias
+//
+//  Created by Sasha on 08/07/2020.
+//  Copyright © 2020 Sasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
